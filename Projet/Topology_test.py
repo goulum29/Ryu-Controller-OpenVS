0c03eba32f21ecf7b0305dfe656b9d165cb2d824
Goulven <goulven.corre@gmail.com>
2019-05-18 18:04:06 +0200

    Modification du script phyton pour rendre la configuration des hotes automatique

--- Projet/Topology_test.py
+++ Projet/Topology_test.py
@@ -20,18 +20,17 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        host1 = self.addHost( 'h1' )
-        host2 = self.addHost( 'h2' )
-        host3 = self.addHost( 'h3' )
+        host1 = self.addHost( 'h1', ip='192.168.10.1/24',defaultRoute='via 192.168.10.254')
+        host2 = self.addHost( 'h2', ip='192.168.20.1/24',defaultRoute='via 192.168.20.254')
+        host3 = self.addHost( 'h3', ip='192.168.30.1/24',defaultRoute='via 192.168.30.254')
         switch1 = self.addSwitch( 's1' )
         switch2 = self.addSwitch( 's2' )
         switch3 = self.addSwitch( 's3' )
         # Add links
-        self.addLink( switch1, host1 )
+        self.addLink( switch1, host1)
         self.addLink( switch2, host2)
         self.addLink( switch3, host3)
         self.addLink( switch1, switch2 )
         self.addLink( switch2, switch3 )
         self.addLink( switch1, switch3 )
-
 topos = { 'mytopo': ( lambda: MyTopo() ) }
